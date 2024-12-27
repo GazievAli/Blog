ec692e85b85c06ef26b1b2ba82e13afcae6d1385
Ali Gaziev <67166171+GazievAli@users.noreply.github.com>
2024-12-27 23:19:48 +0300

    Base structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import "./globals.css";
 import React from "react";
+import Link from "next/link";
+import Header from "@/app/Components/Header/Header";
+import Footer from "@/app/Components/Footer/Footer";
 
 export default function RootLayout({
   children,
@@ -8,8 +11,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-slate-950 text-white">
+        <Header />
         {children}
+        <div className="mt-2 text-gray-400">
+          <p><Link href={"/About"}>About</Link></p>
+          <p><Link href={"/Blog"}>Blog</Link></p>
+          <p><Link href={"/Contacts"}>Contacts</Link></p>
+          <p><Link href={"/Projects"}>Projects</Link></p>
+          <p><Link href={"/Skills"}>Skills</Link></p>
+          <p><Link href={"/"}>Home</Link></p>
+        </div>
+        <Footer />
       </body>
     </html>
   );
